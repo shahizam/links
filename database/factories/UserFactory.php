013d27332adeed7a74dbe401c8fbca776ecae1ce
Shahizam Shafie <shahizamshafie@gmail.com>
2017-10-25 23:20:50 +0800

    Create Link model and edit UserFactory to add Link::class

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,3 +23,11 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Link::class, function (Faker $faker) {
+    return [
+        'title' => $faker->name,
+        'url' => $faker->url,
+        'description' => $faker->paragraph,
+    ];
+});
