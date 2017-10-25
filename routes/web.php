d238192305859afe41a014181f4abe1b0a067dfd
Shahizam Shafie <shahizamshafie@gmail.com>
2017-10-26 01:10:45 +0800

    Add post /submit route

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,22 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/submit', function () {
 	return view('submit');
 });
+
+Route::post('/submit', function(Request $request) {
+	$validator = Validator::make($request->all(), [
+		'title' => 'required|max:255',
+		'url' => 'required|max:255',
+		'description' => 'required|max:255',
+	]);
+	if ($validator->fails()) {
+		return back()
+			->withInput()
+			->withErrors($validator);
+	}
+	$link = new Link;
+	$link->title = $request->title;
+	$link->url = $request->url;
+	$link->description = $request->description;
+	$link->save();
+	return redirect('/');
+});
